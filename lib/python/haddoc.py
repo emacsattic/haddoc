8572e2fb51b9f058f64597f90a4ac03481a27cb0
blais <none@none>
2006-06-05 17:27:54 +0000

    Initial imports.
    --HG--
    extra : convert_revision : svn%3A88dcc788-bdf8-0310-a3ff-bad87a4da1d2/haddoc/trunk%40965

---
+++ lib/python/haddoc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""
+"""
+
+base = '/home/blais/p/conf-local/common/doc/training-cdrom/documentation/python/latest/html'
+baseurl = 'file://%s' % base
+dbfile = '/tmp/docbro'
+
